e92f54039405da337176a96628646d1e281f5c2d
GuedesBruno <brunoguedes@tecassistiva.com.br>
2025-09-11 14:55:15 -0300

    produtos e categorias

---
+++ src/api/banner-site/controllers/banner-site.ts
@@ -0,0 +1,7 @@
+/**
+ * banner-site controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::banner-site.banner-site');
---
+++ src/api/banner-site/services/banner-site.ts
@@ -0,0 +1,7 @@
+/**
+ * banner-site service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::banner-site.banner-site');
